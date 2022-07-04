7437988a224a6094baf118bf6c08bc6cc2e79b31
tarek-elias <tarek.elias97@gmail.com>
2022-07-04 22:46:21 +0400

    improvements #4

--- index.php
+++ index.php
@@ -1,107 +1,145 @@
 <?php
 
 /*** Including Simple HTML DOM Library that will help us to parse the HTML elements from Google page, and to send the HTTP request to Google with a specific keyword */
-include('simple_html_dom.php');
+include 'simple_html_dom.php';
 
-
-
-$html = file_get_html("http://www.google.ae/search?q=amazon&lr=en");
-
-
-$ranking = array();
-$url = array();
-$title = array();
-$description = array();
-$promoted = array();
-
-$temp = array();
-$x = 0;
-$j = 0 ;
-
-
-/**Below is a Foreach loop to iterate over the HTML results and eliminate the Styles elements */
-
-foreach($html->find('<style>') as $style_element){
-  $style_element->remove();
-}
-
-
-
-for($i=0;$i<=50;$i++)
+class GoogleCrawler
 {
-  $item = $html -> find('div[id=main]',0)->childNodes($i);
-  
-  if($item != null AND $item->has_child('span') AND $item->has_child('h3') AND $item->has_child('a') AND $item->has_child('div'))
-  {
+    public $html = null;
+    public $keyword = 'amazon';
 
-    echo 'Yes!' . $i . '<br>';  
-    $temp[$x] = $item;
-    $x++;
-  }
-  else
-  {
-    echo 'no' . $i . '<br>';
-  }
-  //echo 'Child Number: '. $i . '<b>' . $item . '<br>';
-}
+    public $ranking = [];
+    public $url = [];
+    public $title = [];
+    public $description = [];
+    public $promoted = [];
 
-echo '***************************************************************************' . '<br>';
+    public $temp = [];
+    public $x = 0;
+    public $j = 0;
 
-$k2 = 0;
-$clean_res = array();
+    public $k2 = 0;
+    public $clean_res = [];
 
-array_walk($temp, function($v,$k) use ($obj) {
-    if(empty($v)) unset($temp->$k);
-});
-
-
-
-for($k = 0; $k<sizeof($temp); $k++)
-{
-
-  if($temp[$k] != null AND $temp[$k]->has_child('span') AND $temp[$k]->has_child('h3') AND $temp[$k]->has_child('a') AND $temp[$k]->has_child('div') AND $temp[$k]->firstChild() != null AND $temp[$k]->firstChild()->firstChild() != null AND $temp[$k]->firstChild()->firstChild()-> firstChild() != null AND $temp[$k]->firstChild()->firstChild()->firstChild()->nodeName() == 'a')
-  {
-   
-    $clean_res[$k2] = $temp[$k];
-    echo 'Yessssssss!' . $k . $temp[$k] . '<br>' ;
-    $k2++;
-    //$temp[$k]->dump(true);
-    
-  }
-}
-
-
-echo '***************************************************************************' . '<br>';
-
-echo '<br>size of clean array: '. sizeof($clean_res). '<br>';
-
-
-
-
-for($k3=0;$k3<sizeof($clean_res);$k3++)
-{
-    $ranking[$k3] = $k3;
-    $url[$k3] = $clean_res[$k3]->find('a',0)->href;
-    $title[$k3] = $clean_res[$k3]->find('span',0)->plaintext;
-    $description[$k3] = $clean_res[$k3]->find('span[dir=ltr]',2)->plaintext;
-
-}
-
-
-
-    for($k4 = 0; $k4<sizeof($url); $k4++)
+    function call_page($keyword)
     {
-        $url[$k4] = str_replace('/url?q=',"",$url[$k4]);
-        echo $url[$k4]. '<br>';
+        $html = file_get_html("http://www.google.ae/search?q=$keyword&lr=en");
     }
 
-    echo '***************************************************************************' . '<br>';
+    /**Below is a Foreach loop to iterate over the HTML results and eliminate the Styles elements */
+    function remove_styles($html)
+    {
+        foreach ($html->find('<style>') as $style_element) {
+            $style_element->remove();
+        }
+    }
 
-echo 'First Object: <br>';
-for($k5=0;$k5<1;$k5++)
-{
-  echo $ranking[$k5]. '<br>'.$url[$k5]. '<br>'.$title[$k5]. '<br>'. $description[$k5];
+    function phase_one_filtering($html)
+    {
+        for ($i = 0; $i <= 50; $i++) {
+            $item = $html->find('div[id=main]', 0)->childNodes($i);
 
+            if (
+                $item != null and
+                $item->has_child('span') and
+                $item->has_child('h3') and
+                $item->has_child('a') and
+                $item->has_child('div')
+            ) {
+                echo 'Yes!' . $i . '<br>';
+                $temp[$x] = $item;
+                $x++;
+            } else {
+                echo 'no' . $i . '<br>';
+            }
+            //echo 'Child Number: '. $i . '<b>' . $item . '<br>';
+        }
+
+        echo '***************************************************************************' .
+            '<br>';
+    }
+
+    function remove_nulls($temp)
+    {
+        array_walk($temp, function ($v, $k) use ($obj) {
+            if (empty($v)) {
+                unset($temp->$k);
+            }
+        });
+    }
+
+    function phase_two_filtering($temp)
+    {
+        for ($k = 0; $k < sizeof($temp); $k++) {
+            if (
+                $temp[$k] != null and
+                $temp[$k]->has_child('span') and
+                $temp[$k]->has_child('h3') and
+                $temp[$k]->has_child('a') and
+                $temp[$k]->has_child('div') and
+                $temp[$k]->firstChild() != null and
+                $temp[$k]->firstChild()->firstChild() != null and
+                $temp[$k]
+                    ->firstChild()
+                    ->firstChild()
+                    ->firstChild() !=
+                    null and
+                $temp[$k]
+                    ->firstChild()
+                    ->firstChild()
+                    ->firstChild()
+                    ->nodeName() ==
+                    'a'
+            ) {
+                $clean_res[$k2] = $temp[$k];
+                echo 'Yessssssss!' . $k . $temp[$k] . '<br>';
+                $k2++;
+                //$temp[$k]->dump(true);
+            }
+        }
+
+        echo '***************************************************************************' .
+            '<br>';
+
+        echo '<br>size of clean array: ' . sizeof($clean_res) . '<br>';
+    }
+
+    function assign_results($clean_res)
+    {
+        for ($k3 = 0; $k3 < sizeof($clean_res); $k3++) {
+            $ranking[$k3] = $k3;
+            $url[$k3] = $clean_res[$k3]->find('a', 0)->href;
+            $title[$k3] = $clean_res[$k3]->find('span', 0)->plaintext;
+            $description[$k3] = $clean_res[$k3]->find(
+                'span[dir=ltr]',
+                2
+            )->plaintext;
+        }
+    }
+
+    function clean_urls($url)
+    {
+        for ($k4 = 0; $k4 < sizeof($url); $k4++) {
+            $url[$k4] = str_replace('/url?q=', '', $url[$k4]);
+            echo $url[$k4] . '<br>';
+        }
+
+        echo '***************************************************************************' .
+            '<br>';
+    }
+
+    function print_first_object()
+    {
+        echo 'First Object: <br>';
+        for ($k5 = 0; $k5 < 1; $k5++) {
+            echo $ranking[$k5] .
+                '<br>' .
+                $url[$k5] .
+                '<br>' .
+                $title[$k5] .
+                '<br>' .
+                $description[$k5];
+        }
+    }
 }
-
 ?>
